34cbb96df4725205e4e1e7653f77a59de211f186
Khaled100233 <kha.2002.ke@gmail.com>
2025-11-20 10:43:25 +0200

    optimization for render

--- project_de_.py
+++ project_de_.py
@@ -324,4 +324,4 @@ clientside_callback(
 port = int(os.environ.get("PORT", 8080))
 
 if __name__ == "__main__":
-    app.run(debug=True, host = "0.0.0.0", port=port)
+    app.run_server(debug=True, host = "0.0.0.0", port=port)
